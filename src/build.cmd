f7433e54f6a50ea81b215b1b9ef7bec8d5dd5142
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-01-30 15:07:54 +0000

    Added /ALIGN:4096 to linker

--- src/build.cmd
+++ src/build.cmd
@@ -30,7 +30,7 @@
 csc /define:WINDOWS /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 kernel.cs /out:kernel.ilexe /langversion:latest /unsafe || goto Error
 ilc --targetos windows --targetarch x86 kernel.ilexe -g -o kernel.obj --systemmodule kernel --map kernel.map -O || goto Error
 nasm -f win32 -o loader.obj loader.asm || goto Error
-link /debug /subsystem:console /machine:x86 /nodefaultlib /base:0x00200000 /entry:kernel_Program__Main /out:kernel.bin loader.obj kernel.obj || goto Error
+link /debug /subsystem:console /machine:x86 /nodefaultlib /base:0x00200000 /ALIGN:4096 /entry:kernel_Program__Main /out:kernel.bin loader.obj kernel.obj || goto Error
 objcopy -O elf32-i386 kernel.bin kernel.elf || goto Error
 qemu-system-i386 -kernel kernel.elf || goto Error
 
