c4b7d8c5e1485e57dca157e770d8f278ed86d0c4
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-01-31 22:12:28 +0000

    Build output to bin directory

--- src/build.cmd
+++ src/build.cmd
@@ -2,32 +2,34 @@
 :: "Manual" build script that bypasses MSBuild and directly invokes the necessary tools.
 :: 
 
-@del kernel.ilexe >nul 2>&1
-@del kernel.obj >nul 2>&1
-@del kernel.exe >nul 2>&1
-@del kernel.map >nul 2>&1
-@del kernel.pdb >nul 2>&1
-@del kernel.ilk >nul 2>&1
-@del kernel.bin >nul 2>&1
-@del kernel.elf >nul 2>&1
-@del loader.obj >nul 2>&1
+
+:Clean
+rmdir /S /Q ..\bin >nul 2>&1
 
 @if "%1" == "clean" exit /B
 
-csc /define:WINDOWS /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 kernel.cs /out:kernel.ilexe /langversion:latest /unsafe || goto Error
+
+:Build
+mkdir ..\bin
+cd ..\bin
+
+csc /define:WINDOWS /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 ../src/kernel.cs /out:kernel.ilexe /langversion:latest /unsafe || goto Error
 ilc --targetos windows --targetarch x86 kernel.ilexe -g -o kernel.obj --systemmodule kernel --map kernel.map -O || goto Error
-nasm -f win32 -o loader.obj loader.asm || goto Error
+nasm -f win32 -o loader.obj ../src/loader.asm || goto Error
 
 :: QEMU doesn't boot properly when I use the Microsoft linker, continually stuck in a BIOS loop looking for something to boot.
 :: I suspect it's producing a slightly different output than the GNU `ld` command that follows.
 :: link /debug /subsystem:console /machine:x86 /nodefaultlib /base:0x00200000 /ALIGN:4096 /entry:kernel_Program__Main /out:kernel.bin loader.obj kernel.obj || goto Error
 
-ld -m i386pe -T linker.ld -o kernel.bin loader.obj kernel.obj || goto Error
+ld -m i386pe -T ../src/linker.ld -o kernel.bin loader.obj kernel.obj || goto Error
 objcopy -O elf32-i386 kernel.bin kernel.elf || goto Error
 qemu-system-i386 -kernel kernel.elf || goto Error
 
-@goto :EOF
+cd ..\src
+exit /B
+
 
 :Error
 @echo Tool failed.
+cd ..\src
 exit /B 1
--- src/setpath.cmd
+++ src/setpath.cmd
@@ -1,4 +1,4 @@
 
-@set path=C:\Users\frank\.nuget\packages\runtime.win-x64.microsoft.dotnet.ilcompiler\8.0.1\tools;%path%
+@set path=C:\Users\%USERNAME%\.nuget\packages\runtime.win-x64.microsoft.dotnet.ilcompiler\8.0.1\tools;%path%
 @set path=C:\msys64\mingw64\bin;%path%
 @set path=C:\Program Files\qemu;%path%
