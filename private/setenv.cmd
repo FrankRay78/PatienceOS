6cca1f63c41914d4fbe3cd4e4e51a8253e688da7
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-02-06 16:22:16 +0000

    Added shortcut, 'PatienceOS Command Prompt', which I can pin to my Windows taskbar

---
+++ private/setenv.cmd
@@ -0,0 +1,6 @@
+
+:: x64 Visual Studio 2022 Developer Command Prompt
+@call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+:: Custom toolpaths
+@call "D:\Source\Repos\PatienceOS\src\setpath.cmd"
--- src/setpath.cmd
+++ src/setpath.cmd
@@ -1,4 +1,5 @@
 
+:: Custom toolpaths
 @set path=C:\Users\%USERNAME%\.nuget\packages\runtime.win-x64.microsoft.dotnet.ilcompiler\8.0.1\tools;%path%
 @set path=C:\msys64\mingw64\bin;%path%
 @set path=C:\Program Files\qemu;%path%
