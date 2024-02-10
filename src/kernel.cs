7bf525e3005da7ebbdbeb79ce8cb35393e9b2cd1
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-02-10 11:39:14 +0000

    WIP

--- src/kernel.cs
+++ src/kernel.cs
@@ -1,5 +1,12 @@
 using System;
 
+// ______     _   _                      _____ _____ 
+// | ___ \   | | (_)                    |  _  /  ___|
+// | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--. 
+// |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \
+// | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /
+// \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/ 
+
 unsafe class Program
 {
     //https://www.kraxel.org/blog/2018/10/qemu-vga-emulation-and-bochs-display/
@@ -14,54 +21,33 @@ unsafe class Program
 
     static int Main()
     {
-        //Ascii art courtesy of https://www.asciiart.eu/text-to-ascii-art
-        //Doom, width 80
-
-        //Print("______     _   _                      _____ _____ ");
-        //Print("| ___ \\   | | (_)                    |  _  /  ___|");
-        //Print("| |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \\ `--. ");
-        //Print("|  __/ _` | __| |/ _ \\ '_ \\ / __/ _ \\| | | |`--. \\");
-        //Print("| | | (_| | |_| |  __/ | | | (_|  __/\\ \\_/ /\\__/ /");
-        //Print("\\_|  \\__,_|\\__|_|\\___|_| |_|\\___\\___| \\___/\\____/ ");
-
-        //PrintLine(@"______     _   _                      _____ _____ ");
-        //PrintLine(@"| ___ \   | | (_)                    |  _  /  ___|");
-        //PrintLine(@"| |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--. ");
-        //PrintLine(@"|  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \");
-        //PrintLine(@"| | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /");
-        //PrintLine(@"\_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/ ");
-
-
-
-
-
-
-
-
-        //                             ______     _   _                      _____ _____ 
-        //                             | ___ \   | | (_)                    |  _  /  ___|
-        //                             | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--. 
-        //                             |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \
-        //                             | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /
-        //                             \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/ 
-
-
-
-
-
-
-
-
-
-
-
-
-
         Clear();
 
-        PrintLine("Hello");
-        PrintLine("World");
-        PrintLine("Frank");
+
+        //Ascii art courtesy of https://www.asciiart.eu/text-to-ascii-art
+        //Doom font, width 80
+
+        //PrintLine(@" ______     _   _                      _____ _____ ");
+        //PrintLine(@" | ___ \   | | (_)                    |  _  /  ___|");
+        //PrintLine(@" | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--. ");
+        //PrintLine(@" |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \");
+        //PrintLine(@" | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /");
+        //PrintLine(@" \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/ ");
+
+        Print(@"                                                                                ");
+        Print(@"   ______     _   _                      _____ _____                            ");
+        Print(@"   | ___ \   | | (_)                    |  _  /  ___|                           ");
+        Print(@"   | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--.                            ");
+        Print(@"   |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \                           ");
+        Print(@"   | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /                           ");
+        Print(@"   \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/                            ");
+
+
+        //for (int i = 0; i < 81; i++)
+        //{
+        //    Print("a");
+        //}
+
 
         return 0;
     }
@@ -83,6 +69,20 @@ unsafe class Program
     /// </summary>
     unsafe static void PrintLine(string s)
     {
+        Print(s);
+
+        //Increment the row
+        row++;
+
+        //Update the current video address
+        currentVideoAddress = VideoBaseAddress + (row * Width + 2);
+    }
+
+    /// <summary>
+    /// Print a string to the current cursor position
+    /// </summary>
+    unsafe static void Print(string s)
+    {
         fixed (char* ps = s)
         {
             for (int i = 0; i < s.Length; i++)
@@ -93,11 +93,5 @@ unsafe class Program
                 currentVideoAddress += 2;
             }
         }
-
-        //Increment the row
-        row++;
-
-        //Update the current video address
-        currentVideoAddress = VideoBaseAddress + (row * Width + 2);
     }
 }
