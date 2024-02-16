162d56e0f7d9ece96e2570509099ed45331a5b28
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-02-16 21:27:12 +0000

    Extracted video writing logic to Console class

---
+++ src/PatienceOS.Kernel/Console.cs
@@ -0,0 +1,48 @@
+﻿namespace PatienceOS.Kernel
+{
+    /// <summary>
+    /// Writes directly to the video memory
+    /// </summary>
+    /// <remarks>
+    /// Assumes VGA text mode 7 (80 x 25)
+    /// ref: https://en.wikipedia.org/wiki/VGA_text_mode
+    /// </remarks>
+    unsafe public static class Console
+    {
+        //https://www.kraxel.org/blog/2018/10/qemu-vga-emulation-and-bochs-display/
+        private const int Width = 80;
+        private const int Height = 25;
+
+        private const int VideoBaseAddress = 0xb8000;
+
+        private static int pos = 0;
+
+        /// <summary>
+        /// Clear the screen
+        /// </summary>
+        unsafe public static void Clear()
+        {
+            for (int i = 0; i < Width * Height * 2; i++)
+            {
+                *(byte*)(VideoBaseAddress + i) = 0;
+            }
+        }
+
+        /// <summary>
+        /// Print a string to the current cursor position
+        /// </summary>
+        unsafe public static void Print(string s)
+        {
+            fixed (char* ps = s)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    *(byte*)(VideoBaseAddress + pos) = (byte)ps[i];
+                    *(byte*)(VideoBaseAddress + pos + 1) = 0x0f;
+
+                    pos += 2;
+                }
+            }
+        }
+    }
+}
--- src/PatienceOS.Kernel/kernel.cs
+++ src/PatienceOS.Kernel/kernel.cs
@@ -1,58 +1,46 @@
+using PatienceOS.Kernel;
 
 unsafe class Program
 {
-    //https://www.kraxel.org/blog/2018/10/qemu-vga-emulation-and-bochs-display/
-    private const int Width = 80;
-    private const int Height = 25;
+    //static int Main()
+    //{
+    //    var console = new Console();
 
-    private const int VideoBaseAddress = 0xb8000;
-    static int currentVideoAddress = VideoBaseAddress;
+    //    console.Clear();
+
+
+    //    //Ascii art courtesy of https://www.asciiart.eu/text-to-ascii-art
+    //    //Doom font, width 80
+
+    //    console.Print(@"                                                                                ");
+    //    console.Print(@"   ______     _   _                      _____ _____                            ");
+    //    console.Print(@"   | ___ \   | | (_)                    |  _  /  ___|                           ");
+    //    console.Print(@"   | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--.                            ");
+    //    console.Print(@"   |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \                           ");
+    //    console.Print(@"   | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /                           ");
+    //    console.Print(@"   \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/                            ");
+
+
+    //    return 0;
+    //}
 
     static int Main()
     {
-        Clear();
+        Console.Clear();
 
 
         //Ascii art courtesy of https://www.asciiart.eu/text-to-ascii-art
         //Doom font, width 80
 
-        Print(@"                                                                                ");
-        Print(@"   ______     _   _                      _____ _____                            ");
-        Print(@"   | ___ \   | | (_)                    |  _  /  ___|                           ");
-        Print(@"   | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--.                            ");
-        Print(@"   |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \                           ");
-        Print(@"   | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /                           ");
-        Print(@"   \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/                            ");
+        Console.Print(@"                                                                                ");
+        Console.Print(@"   ______     _   _                      _____ _____                            ");
+        Console.Print(@"   | ___ \   | | (_)                    |  _  /  ___|                           ");
+        Console.Print(@"   | |_/ /_ _| |_ _  ___ _ __   ___ ___ | | | \ `--.                            ");
+        Console.Print(@"   |  __/ _` | __| |/ _ \ '_ \ / __/ _ \| | | |`--. \                           ");
+        Console.Print(@"   | | | (_| | |_| |  __/ | | | (_|  __/\ \_/ /\__/ /                           ");
+        Console.Print(@"   \_|  \__,_|\__|_|\___|_| |_|\___\___| \___/\____/                            ");
 
 
         return 0;
-    }
-
-    /// <summary>
-    /// Clear the screen
-    /// </summary>
-    unsafe static void Clear()
-    {
-        for (int i = 0; i < Width * Height * 2; i++)
-        {
-            *(byte*)(VideoBaseAddress + i) = 0;
-        }
-    }
-
-    /// <summary>
-    /// Print a string to the current cursor position
-    /// </summary>
-    unsafe static void Print(string s)
-    {
-        fixed (char* ps = s)
-        {
-            for (int i = 0; i < s.Length; i++)
-            {
-                *(byte*)(currentVideoAddress) = (byte)ps[i];
-                *(byte*)(currentVideoAddress + 1) = 0x0f;
-
-                currentVideoAddress += 2;
-            }
-        }
     }
 }
