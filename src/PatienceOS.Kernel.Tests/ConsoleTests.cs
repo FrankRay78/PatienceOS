c6c3e6e61187a2b3463d3cf70bee2a9f31563ccf
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-02-16 22:44:23 +0000

    Separate solutions for NoStdLib (ie. bootable) and including the .Net runtime (ie. unit test harness)

---
+++ src/PatienceOS.Kernel.Tests/ConsoleTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace PatienceOS.Kernel.Tests
+{
+    public class ConsoleTests
+    {
+        [Fact]
+        public async Task ConsoleShouldThrowAsync()
+        {
+            //Attempts to write directly to the video memory
+            //Assert.Throws<Exception>(() => Console.Clear());
+            //The active test run was aborted. Reason: Test host process crashed : Fatal error. System.AccessViolationException: Attempted to read or write protected memory.
+
+            await Assert.ThrowsAsync<Exception>(() => throw new Exception());
+        }
+    }
+}
