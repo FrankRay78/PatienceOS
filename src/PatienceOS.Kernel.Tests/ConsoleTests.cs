b3150a60276b7166602c2298aa4e9a58e10f71e1
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-04-13 13:51:48 +0100

    Implemented tests for console scrolling when the end of the video memory has been reached

--- src/PatienceOS.Kernel.Tests/ConsoleTests.cs
+++ src/PatienceOS.Kernel.Tests/ConsoleTests.cs
@@ -94,6 +94,87 @@ namespace PatienceOS.Kernel.Tests
                 Assert.Equal((byte)'H', buffer[14]);
                 Assert.Equal((byte)'I', buffer[16]);
             }
+
+            [Fact]
+            public void Should_Scroll_One_Line()
+            {
+                // Given
+                byte* buffer = stackalloc byte[3 * 3 * 2];
+                var frameBuffer = new FrameBuffer(buffer);
+                var console = new Console(3, 3, frameBuffer);
+
+                // When
+                console.PrintLine("AAA");
+                console.PrintLine("BBB");
+                console.PrintLine("CCC");
+                console.PrintLine("DDD");
+
+                // Then
+                Assert.Equal((byte)'B', buffer[0]);
+                Assert.Equal((byte)'B', buffer[2]);
+                Assert.Equal((byte)'B', buffer[4]);
+                Assert.Equal((byte)'C', buffer[6]);
+                Assert.Equal((byte)'C', buffer[8]);
+                Assert.Equal((byte)'C', buffer[10]);
+                Assert.Equal((byte)'D', buffer[12]);
+                Assert.Equal((byte)'D', buffer[14]);
+                Assert.Equal((byte)'D', buffer[16]);
+            }
+
+            [Fact]
+            public void Should_Scroll_Two_Lines()
+            {
+                // Given
+                byte* buffer = stackalloc byte[3 * 3 * 2];
+                var frameBuffer = new FrameBuffer(buffer);
+                var console = new Console(3, 3, frameBuffer);
+
+                // When
+                console.PrintLine("AAA");
+                console.PrintLine("BBB");
+                console.PrintLine("CCC");
+                console.PrintLine("DDD");
+                console.PrintLine("EEE");
+
+                // Then
+                Assert.Equal((byte)'C', buffer[0]);
+                Assert.Equal((byte)'C', buffer[2]);
+                Assert.Equal((byte)'C', buffer[4]);
+                Assert.Equal((byte)'D', buffer[6]);
+                Assert.Equal((byte)'D', buffer[8]);
+                Assert.Equal((byte)'D', buffer[10]);
+                Assert.Equal((byte)'E', buffer[12]);
+                Assert.Equal((byte)'E', buffer[14]);
+                Assert.Equal((byte)'E', buffer[16]);
+            }
+
+            [Fact]
+            public void Should_Scroll_Three_Lines()
+            {
+                // Given
+                byte* buffer = stackalloc byte[3 * 3 * 2];
+                var frameBuffer = new FrameBuffer(buffer);
+                var console = new Console(3, 3, frameBuffer);
+
+                // When
+                console.PrintLine("AAA");
+                console.PrintLine("BBB");
+                console.PrintLine("CCC");
+                console.PrintLine("DDD");
+                console.PrintLine("EEE");
+                console.PrintLine("FFF");
+
+                // Then
+                Assert.Equal((byte)'D', buffer[0]);
+                Assert.Equal((byte)'D', buffer[2]);
+                Assert.Equal((byte)'D', buffer[4]);
+                Assert.Equal((byte)'E', buffer[6]);
+                Assert.Equal((byte)'E', buffer[8]);
+                Assert.Equal((byte)'E', buffer[10]);
+                Assert.Equal((byte)'F', buffer[12]);
+                Assert.Equal((byte)'F', buffer[14]);
+                Assert.Equal((byte)'F', buffer[16]);
+            }
         }
 
         unsafe public class HelloWorld
